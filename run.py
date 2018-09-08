1333b54a62c07e5651e62a3c578468571a09994e
Farbod <farhour@gmail.com>
2018-09-08 16:33:48 +0300

    Finding the longest common sequences and d theinumber of appearances is done. In the next commit, we are going to save the result in both CSV and JSON formats.

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import sys
 import glob
 from tokenizer import tokenize
+import GSuffixTree
 
 path = 'input/sample/*.txt'
 
 countOfArgs = len(sys.argv)
 pathToFiles = list()
+listForGST = list()
 if countOfArgs > 1:
     for i in range(1, countOfArgs):
         pathToFiles.append(sys.argv[i])
@@ -16,4 +18,12 @@ else:
 for pathToFile in pathToFiles:
     with open(pathToFile, 'r') as content_file:
         content = content_file.read()
-        print(tokenize(content))
+    listForGST.append(tokenize(content))
+gst = GSuffixTree.STree(listForGST)
+lcsList = gst.lcs()
+lcsCount = list()
+for lcs in lcsList:
+    lcsCount.append(len(gst.find_all(lcs)))
+print(lcsList)
+print(lcsCount)
+
